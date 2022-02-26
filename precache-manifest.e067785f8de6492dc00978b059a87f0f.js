a7afc11511444ec3e40ec1ec93e09d742f538e49
8358rahul <rahulshanisare@gmail.com>
2022-02-26 07:01:22 +0530

    Updates

---
+++ precache-manifest.e067785f8de6492dc00978b059a87f0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da558d05473134d3f2c6",
+    "url": "/TextUtils-React/static/css/main.f8304a98.chunk.css"
+  },
+  {
+    "revision": "da558d05473134d3f2c6",
+    "url": "/TextUtils-React/static/js/main.da558d05.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/TextUtils-React/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "5ec12183273281da6e26",
+    "url": "/TextUtils-React/static/js/2.5ec12183.chunk.js"
+  },
+  {
+    "revision": "744d7a08b887d0a89df5",
+    "url": "/TextUtils-React/static/js/runtime~main.744d7a08.js"
+  },
+  {
+    "revision": "872e7d202503805feb1fed4748d14e1d",
+    "url": "/TextUtils-React/index.html"
+  }
+];
